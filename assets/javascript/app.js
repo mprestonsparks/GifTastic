8a63a471fe4c873176a468f41138f00ba661b3dd
Preston <mprestonsparks@gmail.com>
2019-03-02 16:15:06 -0600

    added pseudocode in app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Open connection to the Giphy API
+// Create a list of GIF topics to initialize the site with
+// Create buttons from the topics list via loop
+// When user clicks a button, retrieve 10 static, non-animated gif images from the GIPHY API and place them on the page
+// When user clicks an image, the GIF beings to animate
+// When user clicks a gift while animated, pause the GIF/convert back to static image
+// Display the rating of each GIF
+// Push input from search form to the topics list
+// When a topic is searched, give it a button and add to page
