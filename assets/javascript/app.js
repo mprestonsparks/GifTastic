dacfba50004006aa60ef273fe8c3eb8535cd0698
Preston <mprestonsparks@gmail.com>
2019-03-03 18:45:13 -0600

    added createCard(), cleaned up HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,35 +11,62 @@
 
 // Open connection to the Giphy API
 var APIKey = "skQePfC92zigpKm93AxiEg6K4RiNlDTy";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=bill+murray&api_key="+APIKey+"&limit=10"
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    $("#test2").append(response.data[0].rating);
-    // Saving the image_original_url property
-    var imageUrl = response.data[0].url;
-    console.log("imageURL",imageUrl);
-
-    // Creating and storing an image tag
-    var gifImage = $("<img>");
-
-    // Setting the catImage src attribute to imageUrl
-    gifImage.attr("src", imageUrl);
-    gifImage.attr("alt", "gif image");
-
-    // Prepending the catImage to the images div
-    $("#test1").prepend(gifImage);
-
-    $
-    console.log(response);
-  });
 
 // Create a list of GIF topics to initialize the site with
+var topics = ["Miles+Davis","Duke+Ellington","John+Coltrane","Louis+Armstrong","Ella+Fitzgerald"]
+
 // Create buttons from the topics list via loop
+for (i=0; i<topics.length; i++) {
+    var setupButtons = $("<button>");
+    setupButtons.attr("title",topics[i]);
+    setupButtons.attr("id","button"+i);
+    setupButtons.attr("class","btn btn-secondary")
+    setupButtons.attr("type","button")
+    $("#button-section").append(setupButtons);
+    $(setupButtons).text(topics[i]);
+}
 // When user clicks a button, retrieve 10 static, non-animated gif images from the GIPHY API and place them on the page
+$("button").on("click", function() {
+    var buttonSearchTerm = this.title;  
+    var buttonQueryURL = "https://api.giphy.com/v1/gifs/search?q="+buttonSearchTerm+"&api_key="+APIKey+"&limit=10"
+    var buttontest = this.id;
+    console.log("buttontest..",buttontest);
+    rating = "This Rating";
+    imageURL = "https://www.firemagicgrills.com/wp-content/uploads/accessories-small-placeholder.jpg"
+    createCard(rating,imageURL);
+});
+
+    // *** COMMENTED THIS SECTION OUT UNTIL THE CORS ERROR IS FIXED ***
+    // *** ONCE AJAX REQUEST WORKS, REPLACE LAST 3 LINES ABOVE W/ CODE BELOW ***
+    // $.ajax({
+    //     url: buttonQueryURL,
+    //     type: "GET",
+    //     datatype: "jsonp",
+    //     }).then(function(response) {
+    //         for (i=0; i<10; i++) {
+    //             rating = response.data[i].rating;
+    //             imageURL = response.data[i].embed_url;
+    //             createCard(rating,imageURL);
+    //             }
+    //     });
+
+function createCard(rating,imageURL) {
+    var newCard = $("<div>");
+    newCard.attr("class","card");
+    $("#image-section").prepend(newCard);
+    var cardText = $("<h5>");
+    cardText.attr("class","card-header text-center");
+    $(newCard).prepend(cardText);
+    $(cardText).text("Rating: " + rating);
+    var newImg = $("<img>");
+    newImg.attr("class","card-img")
+    newImg.attr("src",imageURL);
+    newImg.attr("alt"," ");
+    $(cardText).append(newImg);
+};
+
 // When user clicks an image, the GIF beings to animate
-// When user clicks a gift while animated, pause the GIF/convert back to static image
+// When user clicks a gif while animated, pause the GIF/convert back to static image
 // Display the rating of each GIF
 // Push input from search form to the topics list
 // When a topic is searched, give it a button and add to page
